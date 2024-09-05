91e5ec054126ea17fc24700d4baf2307bc79cd14
Nathan <nathanlima12@alu.ufc.br>
2024-09-05 19:41:38 -0300

    feat: first commit

---
+++ src/main/java/com/project/productapiredis/ProductApiRedisApplication.java
@@ -0,0 +1,13 @@
+package com.project.productapiredis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductApiRedisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductApiRedisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/productapiredis/ProductApiRedisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.productapiredis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductApiRedisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
